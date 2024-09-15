b0e9f493ea7fdec548cbeebc890a074b926155e9
Pritam Suwal Shrestha <shresthapritam7@gmail.com>
2024-09-15 10:01:18 +0800

    Monster Attack

--- Program.cs
+++ Program.cs
@@ -1,80 +1,39 @@
-﻿// string[] names = { "Alex", "Eddie", "David", "Michael" };
-
-
-// // for (int i = names.Length - 1; i >= 0; i--)
-// // {
-// //     Console.WriteLine(names[i]);
-
-
-// // }
-
-// // foreach (var name in names)
-// // {
-// //     if (name == "David") name = "Sammy";
-// // }
-
-// for (int i = 0; i < names.Length; i++)
-// {
-//     if (names[i] == "David")
-//     {
-//         names[i] = "Sammy";
-//     }
-// }
-
-// foreach (var name in names)
-// {
-//     Console.WriteLine(name);
-// }
-
-// for (int i = 1; i <= 100; i++)
-// {
-//     if (i % 3 == 0 && i % 5 == 0)
-//     {
-//         Console.WriteLine("FizzBuzz");
-//     }
-//     else if (i % 3 == 0)
-//     {
-//         Console.WriteLine("Fizz");
-
-//     }
-
-//     else if (i % 5 == 0)
-//     {
-//         Console.WriteLine("Buzz");
-
-//     }
-
-//     else
-//     {
-//         Console.WriteLine(i);
-//     }
-
-
-// }
-
+﻿int heroScore = 10;
+int monsterScore = 10;
 
 Random random = new Random();
-int current = random.Next(1, 11);
 
-// do
-// {
-//     current = random.Next(1, 11);
-//     Console.WriteLine(current);
-
-// } while (current != 7);
-
-// while (current >= 3)
-// {
-//     Console.WriteLine(current);
-//     current = random.Next(1, 11);
-// }
-
-// Console.WriteLine($"Last number: {current}");
-
-do
+while (heroScore > 0 && monsterScore > 0)
 {
-    current = random.Next(1, 11);
-    if (current >= 8) continue;
 
-    Console.WriteLine(current);
-} while (current != 7);
+
+
+    int randomHeroAttack = random.Next(1, 5);
+    int randomMonsterAttack = random.Next(1, 5);
+
+    if (heroScore > 0)
+    {
+        monsterScore -= randomHeroAttack;
+        Console.WriteLine($"Monster was damaged and lost {randomHeroAttack} health and now has {monsterScore} health.");
+
+        if (monsterScore < 0)
+        {
+            Console.WriteLine("Hero Wins");
+        }
+    }
+
+    if (monsterScore > 0)
+    {
+        heroScore -= randomMonsterAttack;
+        Console.WriteLine($"Hero was damaged and lost {randomMonsterAttack} health and now has {heroScore} health.");
+
+        if (heroScore < 0)
+        {
+            Console.WriteLine("Monster wins.");
+        }
+    }
+
+
+
+
+}
