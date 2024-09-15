c38f130022a330c247b0c1b4a57ace803b2f32f7
Pritam Suwal Shrestha <shresthapritam7@gmail.com>
2024-09-15 09:39:43 +0800

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿// string[] names = { "Alex", "Eddie", "David", "Michael" };
+
+
+// // for (int i = names.Length - 1; i >= 0; i--)
+// // {
+// //     Console.WriteLine(names[i]);
+
+
+// // }
+
+// // foreach (var name in names)
+// // {
+// //     if (name == "David") name = "Sammy";
+// // }
+
+// for (int i = 0; i < names.Length; i++)
+// {
+//     if (names[i] == "David")
+//     {
+//         names[i] = "Sammy";
+//     }
+// }
+
+// foreach (var name in names)
+// {
+//     Console.WriteLine(name);
+// }
+
+// for (int i = 1; i <= 100; i++)
+// {
+//     if (i % 3 == 0 && i % 5 == 0)
+//     {
+//         Console.WriteLine("FizzBuzz");
+//     }
+//     else if (i % 3 == 0)
+//     {
+//         Console.WriteLine("Fizz");
+
+//     }
+
+//     else if (i % 5 == 0)
+//     {
+//         Console.WriteLine("Buzz");
+
+//     }
+
+//     else
+//     {
+//         Console.WriteLine(i);
+//     }
+
+
+// }
+
+
+Random random = new Random();
+int current = random.Next(1, 11);
+
+// do
+// {
+//     current = random.Next(1, 11);
+//     Console.WriteLine(current);
+
+// } while (current != 7);
+
+// while (current >= 3)
+// {
+//     Console.WriteLine(current);
+//     current = random.Next(1, 11);
+// }
+
+// Console.WriteLine($"Last number: {current}");
+
+do
+{
+    current = random.Next(1, 11);
+    if (current >= 8) continue;
+
+    Console.WriteLine(current);
+} while (current != 7);
