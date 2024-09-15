d23025dd67342f5dbe225cdbd453839f379bedd7
Pritam Suwal Shrestha <shresthapritam7@gmail.com>
2024-09-16 07:18:53 +0800

    Data Types

--- Program.cs
+++ Program.cs
@@ -1,39 +1,34 @@
-﻿int heroScore = 10;
-int monsterScore = 10;
-
-Random random = new Random();
-
-while (heroScore > 0 && monsterScore > 0)
-{
+﻿// string? readResult;
 
 
+// do
+// {
+//     Console.WriteLine("Enter a string");
+//     readResult = Console.ReadLine();
 
-    int randomHeroAttack = random.Next(1, 5);
-    int randomMonsterAttack = random.Next(1, 5);
-
-    if (heroScore > 0)
-    {
-        monsterScore -= randomHeroAttack;
-        Console.WriteLine($"Monster was damaged and lost {randomHeroAttack} health and now has {monsterScore} health.");
-
-        if (monsterScore < 0)
-        {
-            Console.WriteLine("Hero Wins");
-        }
-    }
-
-    if (monsterScore > 0)
-    {
-        heroScore -= randomMonsterAttack;
-        Console.WriteLine($"Hero was damaged and lost {randomMonsterAttack} health and now has {heroScore} health.");
-
-        if (heroScore < 0)
-        {
-            Console.WriteLine("Monster wins.");
-        }
-    }
+// } while (readResult == null);
 
 
+Console.WriteLine("Signed integral types:");
+
+Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
 
 
-}
+Console.WriteLine("");
+Console.WriteLine("Unsigned integral types:");
+
+Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+
+
+Console.WriteLine("");
+Console.WriteLine("Floating point types:");
+Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+
